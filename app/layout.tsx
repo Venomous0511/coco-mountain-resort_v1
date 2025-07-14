77da8b23fd365be632a0136c8a9ae45057b073d4
Venomous0511 <johnrenierbaldeviso2@gmail.com>
2025-07-15 00:23:14 +0800

    Added a Icons, Fonts, Images, and Viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,72 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
+import LocalFont from "next/font/Local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Coco Mountain Resort",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+  keywords: ["resort", "mountain", "vacation", "getaway"],
   description: "A serene getaway in the mountains",
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
+const loraSerif = LocalFont({
+  src: [
+    {
+      path: "./fonts/Lora-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Lora-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Lora-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Lora-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Lora-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Lora-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "./fonts/Lora-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Lora-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-lora-serif",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +75,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${loraSerif.variable} font-serif`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
